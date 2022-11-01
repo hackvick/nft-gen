1182c2cf49fe8a3d50e076b7ee7b0166779e74dc
vicky <vickyhasija167@gmail.com>
2022-11-01 18:30:44 +0530

    m

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 // import './App.css';
 import "./style/styles.css"
 import {HomePage} from './MyComponents/homepage/HomePage';
+import {BrowserRouter as Router,Route,Routes} from 'react-router-dom'
 
 function App() {
   return (
    <>
-    <HomePage/>
+   <Router>
+    <Routes>
+      <Route path="/" element={<HomePage />}></Route>
+    </Routes>
+   </Router>
+  
    </>
   );
 }
