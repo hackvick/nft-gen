eb72018368b8328e56fc659bd38a79b540ce1d28
gkashyap9602 <gkashyap9602@gmail.com>
2022-11-07 18:02:59 +0530

    download nft

--- src/MyComponents/GetStarted/GenerateNFTCollection/GenerateNFTCollection.jsx
+++ src/MyComponents/GetStarted/GenerateNFTCollection/GenerateNFTCollection.jsx
@@ -9,18 +9,25 @@ import { NftGenerator } from "../../modals/generateNft/generateNft";
 import EditData from "../../modals/EditData";
 import { Rarity } from "../Rarity.js/Rarity";
 import { useNftProvider } from "../../context/NftProvider";
-import {API_BASE_URL} from "../../../Api/Api"
+import { API_BASE_URL } from "../../../Api/Api";
 // import { toast } from "react-toastify";
 // import axios from "axios";
 // import cross from "../../../assets/cross.png";
 
 export const GenerateNFT = (props) => {
-  const { layerData ,selectedLayerName,getImageData,setGetImageData,getImages} = props
+  const {
+    layerData,
+    selectedLayerName,
+    getImageData,
+    setGetImageData,
+    getImages,
+  } = props;
   const { layerId, setLoader } = useNftProvider();
   const [show, setShow] = useState(false);
   const [toggle, setToggle] = useState(false);
   const [edit, setEdit] = useState(false);
   const [uploadData, setUploadData] = useState([]);
+  const [localUploadImages, setLocalUploadImages] = useState();
 
   useEffect(() => {
     // Fetching other layer images when LayerId changes
@@ -33,12 +40,39 @@ export const GenerateNFT = (props) => {
   }, [layerId]);
 
   const handleShow = () => setToggle(true);
+  
+  let imagess = []
+  const onImageChange = (event) => {
+    event.preventDefault();
+    console.log(event.target.name, "e target name upload image side");
+    console.log(event.target.name.files, "e target files upload image side");
+    // let formData = new FormData(event.target);
+    // console.log(formData,"formData upload onImageChange fn side");
+    let files = event.target.name.files;
+    console.log(files, "selectedImages onimagechange fn side");
+
+    if (event.target.name.files && event.target.name.files[0]) {
+      for (let i = 0; i < files.length; i++) {
+        console.log(files[i], "files loop side ");
+        imagess.push(URL.createObjectURL(files[i]))
+        console.log(URL.createObjectURL(files[i]),"file url created")
+
+        setLocalUploadImages(URL.createObjectURL(files[i]));
+      }
+    }
+  };
 
   return (
     <div className={style.nftGenerate}>
+      {console.log(
+        localUploadImages,
+        "upload local generatenft collection side"
+      )}
+      {console.log(imagess,"imagess after push ")}
       {/* {console.log(layerData, "layerData genrate nft collection side ")} */}
-       {/* {console.log(selectedLayerName,"selectedLayerNAme generatenft side")} */}
+      {/* {console.log(selectedLayerName,"selectedLayerNAme generatenft side")} */}
       <UploadImage
+        onImageChange={onImageChange}
         setUploadData={setUploadData}
         getImages={getImages}
         setShow={setShow}
@@ -53,36 +87,45 @@ export const GenerateNFT = (props) => {
             <div className={style.displaySetting}>
               <ul className={style.categoryList}>
                 {getImageData.map((layerImg, i) => (
-                  <>
-                    {/* {console.log(layerImg.imageUrl, "layerimagedata")} */}
-                    <li key={i}>
-                      <div className={style.hidenft}>
-                        <span className={style.layerImages}>
-                          <img
-                            src={`${API_BASE_URL}${layerImg.imageUrl}`}
-                            alt="plus"
-                          />
-                        </span>
-                      </div>
-                    </li>
-                  </>
+                <>
+                  {/* {console.log(layerImg.imageUrl, "layerimagedata")} */}
+                  <li key={"i"}>
+                    <div className={style.hidenft}>
+                      <span className={style.layerImages}>
+                        <img
+                          src={`${API_BASE_URL}${layerImg.imageUrl}`}
+                          // src={localUploadImages}
+                          alt="plus"
+                        />
+                      </span>
+                    </div>
+                  </li>
+                </>
                 ))}
                 <li>
-                  <div
-                    className={style.hidenft}
-                    onClick={() => {
-                      setShow(true);
-                    }}
-                  >
-                    <label className={style.nftCursor}>
-                      <span className={style.uploadImage}>
-                        <img src={plus} alt="plus" />
-                        <span className={style.photoNft}>
-                          Upload Layer Image
-                        </span>
-                      </span>
-                    </label>
-                  </div>
+                  {/* upload Image div starts here that trigger upload modal */}
+                  {layerData.length !== 0 ? (
+                    <>
+                      <div
+                        className={style.hidenft}
+                        onClick={() => {
+                          setShow(true);
+                        }}
+                      >
+                        <label className={style.nftCursor}>
+                          <span className={style.uploadImage}>
+                            <img src={plus} alt="plus" />
+                            <span className={style.photoNft}>
+                              Upload Layer Image
+                            </span>
+                          </span>
+                        </label>
+                      </div>
+                    </>
+                  ) : (
+                    ""
+                  )}
+                  {/* upload Image div ends here that trigger upload modal */}
                 </li>
               </ul>
               <div className={style.LayerSettingBottom}>
@@ -108,15 +151,20 @@ export const GenerateNFT = (props) => {
                   </div>
                 </div>
                 <div className={style.layerBottomSection}>
+                  {/* show layer settings when getLayer Api Hit and Data retreive successfully */}
                   {layerData.length !== 0 ? (
                     <>
                       <div className={style.setting}>
                         <h2>Layer Settings</h2>
                       </div>
-                      
+
                       <div className={style.currentLayer}>
                         <label className={style.labelLayer}>Layer Name</label>
-                        <input className="form-control" type="text" value={selectedLayerName} />
+                        <input
+                          className="form-control"
+                          type="text"
+                          value={selectedLayerName}
+                        />
                       </div>
 
                       <div className={style.raritySetting}>
--- src/MyComponents/modals/UploadImage/UploadImage.js
+++ src/MyComponents/modals/UploadImage/UploadImage.js
@@ -6,19 +6,39 @@ import style from "./UploadImage.module.css";
 import { toast } from "react-toastify";
 import { useNftProvider } from "../../context/NftProvider";
 import { UPLOAD_IMAGES_WITH_LAYER_ID } from "../../../Api/Api";
-const UploadImage = ({ show, setShow, getImages, setUploadData }) => {
+const UploadImage = ({ show, setShow, getImages, setUploadData,onImageChange }) => {
   const { layerId, setLoader } = useNftProvider();
   // const [file, setFile] = useState(null);
   // const [upload, setUpload] = useState(false);
   // const [download, setDownload] = useState(false);
   // const [addLayer, setAddLayer] = useState(false);
-
   const handleClose = () => setShow(false);
+
+
+  // const onImageChange = (event) => {
+  //   event.preventDefault();
+  //   console.log(event.target.name,"e target name upload image side")
+  //   console.log(event.target.name.files,"e target files upload image side")
+  //   // let formData = new FormData(event.target);
+  //   // console.log(formData,"formData upload onImageChange fn side");
+  //   let selectedImages = event.target.name.files
+    
+  //   if (event.target.name.files && event.target.name.files[0]) {
+  //     // eslint-disable-next-line array-callback-return
+  //     // selectedImages.map((UploadedImage)=>{
+  //       setLocalUploadImages(URL.createObjectURL(event.target.name.files[0]));
+
+  //     // })
+  //   }
+  //  }
+
 
   const uploadLayerImages = (e) => {
     const token = localStorage.getItem("token");
     e.preventDefault();
     let formData = new FormData(e.target);
+    console.log(formData,"formData uploadlayerimages fn side");
+
     setLoader(true);
     axios
       .post(`${UPLOAD_IMAGES_WITH_LAYER_ID}${layerId}`, formData, {
@@ -53,11 +73,12 @@ const UploadImage = ({ show, setShow, getImages, setUploadData }) => {
           <form
             encType="multipart/form-data"
             onSubmit={(e) => {
+              // onImageChange(e)
               uploadLayerImages(e);
             }}
           >
             <Modal.Body className={style.uploadBody}>
-              <input type="file" name="name" />
+              <input  type="file" name="name" multiple="multiple" />
             </Modal.Body>
             <Modal.Footer className={style.uploadFooter}>
               <Button
